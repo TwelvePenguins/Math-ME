41139a185458d4fa9ad0a9990c22431e380d9ba9
Du Yuhan <yuhan.du@gmail.com>
2025-01-06 14:47:23 +0800

    Add JPL consolidation for other attributes

--- jpl-ephem.py
+++ jpl-ephem.py
@@ -12,16 +12,19 @@ from astropy.coordinates import solar_system_ephemeris
 solar_system_ephemeris.set('jpl')
 
 base_time = datetime(1958, 10, 13, 2, 0, 0)
-coords_arr = []
 
 sum_table = pd.read_excel("sum_table.xlsx") # Import data from excel spreadsheet
 timestamps = sum_table["Time"] # Take the column of data labelled "Time"
+calc_distances = sum_table[["Time", "Spherical Coordinates"]] # Import the calculated data from the excel
 
 print(timestamps)
 
-def get_moon(time_input: list):
+def get_moon(time_input: list, attribute: str): # Returns dataframe of the position of the moon in terms of the specified attribute. Available for distance, right ascension, declination.
+    
+    coords_arr = []
 
     for seconds in time_input: 
+
         # Turn time, which is in seconds, into year:month:day hr:min:sec
         new_time = base_time + timedelta(seconds=seconds)
         new_time = new_time.strftime("%Y-%m-%d %H:%M:%S")
@@ -29,11 +32,25 @@ def get_moon(time_input: list):
         new_time = Time(new_time, format="iso", scale="utc") # Convert time input to astropy time
         coords = coordinates.get_body("moon", new_time, ephemeris="de432s") # Get coordinates
         coords.representation_type = "spherical" # Change coordinates to spherical
-        coords_arr.append(coords.distance.value)
 
-    return coords_arr
+        if attribute.lower() == "distance": 
+            coords_arr.append(coords.distance.value)
+        elif attribute.lower() == "ra": 
+            coords_arr.append(np.deg2rad(coords.ra.value))
+        elif attribute.lower() == "dec": 
+            coords_arr.append(np.deg2rad(coords.dec.value))
+    
+    # Generate dataframe with data
+    jpl_data = pd.DataFrame(coords_arr, columns=[attribute]) 
+    jpl_data = pd.concat([timestamps, jpl_data], axis=1)
 
-jpl_distances = pd.DataFrame(get_moon(timestamps), columns=["Distance"])
-jpl_distances = pd.concat(timestamps, jpl_distances)
+    return jpl_data
 
-print(jpl_distance)
+jpl_distance = get_moon(timestamps, "Distance")
+jpl_ra = get_moon(timestamps, "RA")
+jpl_dec = get_moon(timestamps, "Dec")
+
+jpl_ephem = pd.concat([jpl_distance, jpl_ra, jpl_dec], axis=1, join="inner") # Consolidate data
+jpl_ephem = jpl_ephem.T.drop_duplicates().T # Drop duplicated columns
+
+jpl_ephem.to_excel("jpl_ephem.xlsx")
