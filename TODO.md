59f8109c743fcd2a469b7734da3ea249b571755d
TwelvePenguins <yuhan.du@gmail.com>
2025-01-08 21:05:49 +0800

    Fix terminology & standardise azimuth range

--- TODO.md
+++ TODO.md
@@ -1,3 +1,2 @@
 1. Fix the annotation function
 2. Add verification for error
-3. Figure out why the moon is the wrong way
--- jpl-ephem.py
+++ jpl-ephem.py
@@ -1,6 +1,7 @@
 import numpy as np
 import pandas as pd
 import math as m
+from matplotlib import pyplot as plt 
 from datetime import datetime, timedelta
 from fractions import Fraction as fr
 
@@ -12,7 +13,8 @@ import astropy.coordinates.representation as r
 from astropy.coordinates import solar_system_ephemeris
 solar_system_ephemeris.set('jpl')
 
-base_time = datetime(1958, 10, 13, 2, 0, 0)
+BASE_TIME = datetime(1958, 10, 13, 2, 0, 0)
+TIME_ZERO = "1958-10-13 02:00:00"
 
 sum_table = pd.read_excel("sum_table.xlsx") # Import data from excel spreadsheet
 timestamps = sum_table["Time"] # Take the column of data labelled "Time"
@@ -22,7 +24,7 @@ def get_moon(time_input: list, attribute: str): # Returns dataframe of the posit
     coords_arr = []
 
     # Find the longitude of the body at t=0 to create offset
-    longitude_offset = coordinates.get_body("moon", Time("1958-10-13 02:00:00", format="iso", scale="utc"), ephemeris="de432s")
+    longitude_offset = coordinates.get_body("moon", Time(TIME_ZERO, format="iso", scale="utc"), ephemeris="de432s")
     longitude_offset = longitude_offset.transform_to(coordinates.GeocentricMeanEcliptic())
     longitude_offset = np.deg2rad(longitude_offset.lon.value)
     print(longitude_offset)
@@ -30,7 +32,7 @@ def get_moon(time_input: list, attribute: str): # Returns dataframe of the posit
     for seconds in time_input: 
 
         # Turn time, which is in seconds, into year:month:day hr:min:sec
-        new_time = base_time + timedelta(seconds=seconds)
+        new_time = BASE_TIME + timedelta(seconds=seconds)
         new_time = new_time.strftime("%Y-%m-%d %H:%M:%S")
 
         new_time = Time(new_time, format="iso", scale="utc") # Convert time input to astropy time
@@ -42,8 +44,11 @@ def get_moon(time_input: list, attribute: str): # Returns dataframe of the posit
             coords_arr.append(coords.distance.value)
         elif attribute.lower() == "azimuth angle": 
             coords_arr.append(fr(1, 2) * np.pi - np.deg2rad(coords.lat.value))
-        elif attribute.lower() == "polar angle": # We need to perform an offset for longitude to adjust for the different x-axis between the dataset and the calculated coordinates.
-            coords_arr.append(np.deg2rad(coords.lon.value) - longitude_offset)
+        elif attribute.lower() == "polar angle": # We need to perform an offset for longitude to adjust for the different x-axis between the dataset and the calculated coordinates, and change the range to be 0 to 2pi instead of pi to -pi
+            longitude = np.deg2rad(coords.lon.value) - longitude_offset
+            if longitude < 0: 
+                longitude = 2 * np.pi + longitude
+            coords_arr.append(longitude)
     
     # Generate dataframe with data
     jpl_data = pd.DataFrame(coords_arr, columns=[attribute]) 
@@ -59,3 +64,17 @@ jpl_ephem = pd.concat([jpl_distance, jpl_pol, jpl_az], axis=1, join="inner") # C
 jpl_ephem = jpl_ephem.T.drop_duplicates().T # Drop duplicated columns
 
 jpl_ephem.to_excel("jpl_ephem_transformed.xlsx")
+
+# jpl_ephem = pd.read_excel("jpl_ephem_transformed.xlsx")
+
+# jpl_ephem_norm = pd.read_excel("jpl_ephem.xlsx")
+
+# fig_r = plt.figure()
+# axes_t = fig_r.add_subplot()
+
+# fig_r = plt.plot(jpl_ephem["Time"], jpl_ephem["Polar Angle"])
+# fig_r = plt.plot(sum_table["Time"], sum_table["Polar Angle"])
+# fig_r = plt.ylabel("Lunar straight line distance from Earth / km")
+# fig_r = plt.xlabel("Time / s")
+
+# plt.show()
